0cc7d7ae103d0454d3c62ede85bccd79ad8c3b29
Brad Bell <brad.bb.bell@gmail.com>
2024-10-30 15:57:53 -0600

    adds favicon

--- includes/header.php
+++ includes/header.php
@@ -8,17 +8,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $title; ?></title>
     <link rel="stylesheet" href="/php-proj/assets/css/style.css">
+    <link rel="icon" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>📈</text></svg>">
 </head>
 <header class="bg-purple-400 text-black p-4 w-full">
     <div class="max-w-screen-lg mx-auto flex">
-        <span class="">📈 Contributions 📈</span>
+        <span class="font-semibold">📈 Contributions 📈</span>
         <nav class="ml-12">
             <ul class="list-none p-0 m-0">
                 <li class="inline mr-4">
-                    <a href="/php-proj/public/index.php" class="text-black no-underline">Home</a>
+                    <a href="/php-proj/public/index.php" class="text-gray-800 hover:text-black no-underline">Home</a>
                 </li>
                 <li class="inline mr-4">
-                    <a href="/php-proj/public/transactions.php" class="text-black no-underline">Transactions</a>
+                    <a href="/php-proj/public/transactions.php" class="text-gray-800 hover:text-black no-underline">Transactions</a>
                 </li>
             </ul>
         </nav>
