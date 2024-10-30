cef13cb988f1dc208c2f7899cd7c0bf55feb7b45
Brad Bell <brad.bb.bell@gmail.com>
2024-10-30 11:35:42 -0600

    changes php version to 8.2 for prettier

--- includes/header.php
+++ includes/header.php
@@ -9,16 +9,19 @@
     <title><?php echo $title; ?></title>
     <link rel="stylesheet" href="/php-proj/assets/css/style.css">
 </head>
-<header class="text-center bg-[#dc91e2] text-black p-4">
-    <nav>
-        <ul class="list-none p-0 m-0">
-            <li class="inline mr-4">
-                <a href="/php-proj/public/index.php" class="text-black no-underline">Home</a>
-            </li>
-            <li class="inline mr-4">
-                <a href="/php-proj/public/about.php" class="text-black no-underline">About</a>
-            </li>
-        </ul>
-    </nav>
+<header class="bg-[#dc91e2] text-black p-4 w-full">
+    <div class="max-w-screen-lg mx-auto flex">
+        <span class="">📈 Contributions 📈</span>
+        <nav class="ml-12">
+            <ul class="list-none p-0 m-0">
+                <li class="inline mr-4">
+                    <a href="/php-proj/public/index.php" class="text-black no-underline">Home</a>
+                </li>
+                <li class="inline mr-4">
+                    <a href="/php-proj/public/about.php" class="text-black no-underline">About</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
 </header>
 <main class="p-8 max-w-screen-lg mx-auto">
