9b9070da49ffee4455e8b614129212295cae1459
Brad Bell <brad.bb.bell@gmail.com>
2024-10-30 09:13:30 -0600

    adds sql to create db and add table

---
+++ config/create_table.php
@@ -0,0 +1,31 @@
+<?php
+$servername = 'localhost';
+$username = 'root';
+$password = '';
+$dbname = 'contributions';
+
+try {
+    $database = new PDO("mysql:host=$servername;", $username, $password);
+    $database->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = "CREATE DATABASE IF NOT EXISTS $dbname";
+    $database->exec($sql);
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
+
+try {
+    $database = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $database->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = "CREATE TABLE IF NOT EXISTS Transactions (
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    transaction_date DATE NOT NULL,
+    account VARCHAR(30) NOT NULL,
+    account_type VARCHAR(30) NOT NULL,
+    amount DOUBLE NOT NULL
+    )";
+    $database->exec($sql);
+} catch (PDOException $e) {
+    echo 'Error creating table';
+    echo $e->getMessage();
+}
+?>
--- public/index.php
+++ public/index.php
@@ -1,9 +1,43 @@
 <?php
-    require_once '../includes/header.php';
+require_once '../config/create_table.php';
+require_once '../includes/header.php';
 ?>
 
 <h1 class="text-center">Welcome to Contributions</h1>
-<p>Content will be loaded here</p>
+<table class="border-2 border-black w-full">
+    <thead>
+        <tr>
+            <th>Date</th>
+            <th>Account</th>
+            <th>Type</th>
+            <th>Amount</th>
+        </tr>
+    </thead>
+    <tbody>
+    <tr>
+        <td>10/30/2024</td>
+        <td>TIAA</td>
+        <td>Mutual Fund</td>
+        <td>$100</td>
+    </tr>
+    <tr>
+        <td>10/30/2024</td>
+        <td>TIAA</td>
+        <td>Mutual Fund</td>
+        <td>$100</td>
+    </tr>
+    <tr>
+        <td>10/30/2024</td>
+        <td>TIAA</td>
+        <td>Mutual Fund</td>
+        <td>$100</td>
+    </tr>
+    <tr>
+        <td colspan="3">Total</td>
+        <td >$300</td>
+    </tr>
+    </tbody>
+</table>
 
 
 <?php require_once '../includes/footer.php'; ?>
