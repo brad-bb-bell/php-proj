86d66586440a5a4ed11795abf42a175da25efc16
Brad Bell <brad.bb.bell@gmail.com>
2024-10-30 20:08:04 -0600

    lil changes

--- includes/header.php
+++ includes/header.php
@@ -11,7 +11,7 @@
     <link rel="icon" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>📈</text></svg>">
 </head>
 <header class="bg-purple-400 text-black p-4 w-full">
-    <div class="max-w-screen-lg mx-auto flex">
+    <div class="max-w-screen-lg mx-auto flex items-center">
         <span class="font-semibold">📈 Contributions 📈</span>
         <nav class="ml-12">
             <ul class="list-none p-0 m-0">
